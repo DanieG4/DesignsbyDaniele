326249ef426762eaf6b258bccda20469126cfe9e
DanieG4 <teacherdaniele4@gmail.com>
2025-05-10 09:14:59 -0600

    indv changes to pages

---
+++ server.js
@@ -0,0 +1,8 @@
+
+$(document).ready(function () {
+    $("#contact-form").on('submit', function (event) {
+        event.preventDefault()
+        console.log('success') 
+    //    console.log($(".contact-inputs")) 
+    })
+})
